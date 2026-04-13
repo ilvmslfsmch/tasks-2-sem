1cafe98fe53d9cefd160d2306da3cb626f499338
Мартини Персей ТКИ-141 <pmartini@yandex.ru>
2026-04-13 16:27:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,34 +18,32 @@ int main () {
     cout << "Select input method: by points - " << BY_POINTS << ", by coordinates - " << BY_COORDINATES << "." << endl;
     cin >> choice;
 
-    Hexagon* hex = nullptr;
+    Hexagon hex;
 
     switch (choice) {
         case BY_POINTS: {
             Point p1, p2, p3, p4, p5, p6;
             cout << "Enter 6 points (x y x y ...):" << endl;
             cin >> p1 >> p2 >> p3 >> p4 >> p5 >> p6;
-            hex = new Hexagon(p1, p2, p3, p4, p5, p6);
+            hex = Hexagon(p1, p2, p3, p4, p5, p6);
             break;
         }
             case BY_COORDINATES: {
             double x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6;
             cout << "Enter 12 coordinates (x1 y1 x2 y2 ...):" << endl;
             cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> x5 >> y5 >> x6 >> y6;
-            hex = new Hexagon(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6);
+            hex = Hexagon(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6);
             break;
         }
             default:
             cout << "Error. Invalid choice." << endl;
             return 1;
     }
-    if (hex != nullptr) {
-        cout << *hex << endl;
-        cout << "Area: " << hex->getArea() << endl;
-        cout << "Perimeter: " << hex->getPerimeter() << endl;
-        cout << "Radius:" << hex->getRadius() << endl;
+    cout << hex << endl;
+    cout << "Area: " << hex.getArea() << endl;
+    cout << "Perimeter: " << hex.getPerimeter() << endl;
+    cout << "Radius:" << hex.getRadius() << endl;
 
-        delete hex;
     }
     return 0;
 }
